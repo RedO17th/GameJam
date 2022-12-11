89b7aa8233b12cff1ad6e4048d0ea19c9931fc0e
HeBaJI9ILIJKa <shestera.lk@gmail.com>
2022-12-11 18:09:28 +0600

    EnemySpawner.Part Vl

--- Assets/Scripts/Player/CloseCombatAbility.cs
+++ Assets/Scripts/Player/CloseCombatAbility.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class CloseCombatAbility : BaseAbility
 {
+    [SerializeField] private GameObject _CombatAtackZone;
     public override void Use()
     {
         Debug.Log($"CloseCombatAbility.Use");
+        //включить триггер
+        _CombatAtackZone.SetActive(true);
     }
 }
