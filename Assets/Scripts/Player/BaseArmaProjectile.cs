5e06d944c932ca26a24f987e4aa4717ca7233de2
HeBaJI9ILIJKa <shestera.lk@gmail.com>
2022-12-11 20:06:20 +0600

    build1.Part II
    lifetime пулек,
    удалил лишний префаб

--- Assets/Scripts/Player/BaseArmaProjectile.cs
+++ Assets/Scripts/Player/BaseArmaProjectile.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class BaseArmaProjectile : MonoBehaviour
 {
-    [SerializeField] private float _speed = 5f;
+    [SerializeField] private float _speed = 5f, _lifeTime = 5f;
     private Rigidbody _rigidBody = null;
     
 
     private void Awake()
     {
         _rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        Invoke("AutoDestroy", _lifeTime);  
+    }
+
+    private void AutoDestroy()
+    {
+        Destroy(gameObject);
     }
 
     public void Launch()
